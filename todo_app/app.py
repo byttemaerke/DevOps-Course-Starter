882b8ea9e392b7f8b0f5f5c3694d8c3dae9406dd
byttemaerke <sboudema@protonmail.com>
2021-04-26 10:49:00 +0100

    Creates new todo items

--- todo_app/app.py
+++ todo_app/app.py
@@ -1,13 +1,15 @@
-from flask import Flask, render_template
-from todo_app.data.session_items import get_items
+from flask import Flask, render_template, request
+from todo_app.data.session_items import get_items, add_item
 from todo_app.flask_config import Config
 
 app = Flask(__name__)
 app.config.from_object(Config)
 
 
-@app.route('/')
+@app.route('/', methods=['GET', 'POST'])
 def index():
+    if request.method == 'POST':
+        add_item(request.form.get('title'))
     return render_template('index.html', items = get_items())
 
 
